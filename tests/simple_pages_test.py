128ab813da9d5203a58e5d574971bb1a6c1f0f1f
Angela Arroyo <ara55@njit.edu>
2022-02-25 01:46:52 -0500

    edited simple pages test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,8 +4,8 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">Home</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">About</a>' in response.data
+    assert b'<a class="nav-link" href="/index.html">Home</a>' in response.data
+    assert b'<a class="nav-link" href="/about.html">About</a>' in response.data
 
 
 def test_request_index(client):
