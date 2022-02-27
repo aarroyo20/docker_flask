4a0e5ae093bc3489d9dba696908b7030bf28874c
Angela Arroyo <ara55@njit.edu>
2022-02-26 21:29:47 -0500

    edited simple pages test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" aria-current="page" href="about.html">About</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/about">About</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
